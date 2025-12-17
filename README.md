c237347f73d410ad8df4e948e6c49afb86273a0a
Massimo Cassandro <massimo.cassandro@gmail.com>
2025-12-17 14:07:15 +0100

    v.1.0.2 - Setup

---
+++ README.md
@@ -0,0 +1,11 @@
+# Dom builder
+
+Builds dom elements from array of objects.
+
+Only available as module script.
+
+```
+import { domBuilder } from '@massimo.cassandro/dom-builder';
+
+domBuilder({ /* options */}); // see source code for details
+```
---
+++ src/domBuilderBasicSetup.js
@@ -0,0 +1,39 @@
+
+/**
+ * Esegue le operazione di base del domBuilder: assegnazione classi, id ecc...
+ */
+export function domBuilderBasicSetup (element, domBuilderItem) {
+
+  // classi
+  // possibili denominazioni: class o className
+  // se presente className, class viene ignorato
+  if (domBuilderItem.class && !domBuilderItem.className) {
+    domBuilderItem.className = domBuilderItem.class;
+  }
+
+  // se item.attrs è un array singolo viene trattato come singolo elemento
+  if (domBuilderItem.attrs && Array.isArray(domBuilderItem.attrs) && !Array.isArray(domBuilderItem.attrs[0])) {
+    domBuilderItem.attrs = [ domBuilderItem.attrs ];
+
+  // item.attrs può essere un oggetto del tipo {attr_name: attr_value}
+  } else if (typeof domBuilderItem.attrs === 'object' && !Array.isArray(domBuilderItem.attrs) && domBuilderItem.attrs !== null) {
+    domBuilderItem.attrs = Object.entries(domBuilderItem.attrs);
+  }
+
+  (domBuilderItem.attrs ?? []).forEach(attr => {
+    if (attr[1] != null) {
+      element.setAttribute(attr[0], String(attr[1]));
+    }
+  });
+
+  // assegnazioen classi e id (dopo attrs in modo che le proprietà a livello di oggetto prevalgano)
+  if(domBuilderItem.className) {
+    element.className = domBuilderItem.className;
+  }
+  if(domBuilderItem.id) {
+    element.id = domBuilderItem.id;
+  }
+
+  return element;
+
+}
